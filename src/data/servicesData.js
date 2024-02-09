69dc17fff4c414e86f0539e89e321aac7441c2e8
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-02-10 01:51:11 +0300

    Update servicesData.js

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -7,67 +7,36 @@ import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, F
  
 
 export const servicesData = [
-    // {
-    //     id: 1,
-    //     title: 'Marketing',
-    //     icon: <BiShoppingBag/>
-    // },
     {
         id: 2,
-        title: 'Content Writing',
+        title: 'Fullstack Developer',
         icon: <BiPencil /> 
     },
     {
         id: 3,
-        title: 'Email Marketing',
+        title: 'AWS Amplify',
         icon: <AiOutlineMail />
     },
-    // {
-    //     id: 4,
-    //     title: 'Internet Research',
-    //     icon: <FaInternetExplorer />
-    // },
-    // {
-    //     id: 5,
-    //     title: 'Tutoring',
-    //     icon: <FaChalkboardTeacher />
-    // },
-
     {
         id: 6,
-        title: 'Web Design',
+        title: AWS Database DaaS',
         icon: <BsCodeSlash />
     },
     {
         id: 7,
-        title: 'Photography',
+        title: 'Master of Version Control',
         icon: <FaCameraRetro />
     },
-    // {
-    //     id: 8,
-    //     title: 'Pinterest Virtual Assistant',
-    //     icon: <FaPinterest />
-    // }, 
     {
         id: 9,
-        title: 'Video Editing',
+        title: 'Deadline Dynamo',
         icon: <FaVideo />
     },
-    // {
-    //     id: 10,
-    //     title: 'Data Entry', 
-    //     icon: <BsClipboardData />
-    // },
     {
         id: 11,
         title: 'Ebook Writing',
         icon: <FaTabletAlt />
     },
-    // {
-    //     id: 12,
-    //     title: 'Audio Transcription',
-    //     icon: <AiFillAudio />
-    // },
     {
         id: 13,
         title: 'Blog Posting',
@@ -75,7 +44,3 @@ export const servicesData = [
     },
 
 ]
-
-// Uncomment your required service.
-// Couldn't find the required services? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-// You can also add on your own 😉.
