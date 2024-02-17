1d73620c8f8efc5a8f341e9975c1ce181f7e72ad
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-02-17 04:21:36 +0300

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
-    image: 2
+    description1: "My name's Noel. I'm a software engineer and fullstack developer fron Nairobi, Kenya.",
+    description2: "By day, I lead development projects at a local agency, and during the evenings, I delve into freelance work, building personal projects, and exploring the realms of machine learning and AI. In my leisure time, I indulge in writing articles, poetry, and consistently seek opportunities to learn and venture into new domains.",
+    image: 1
 }
