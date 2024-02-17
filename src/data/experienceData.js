010328d3bfb51fba1439ba7990613a0abc4f8276
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-02-17 04:35:09 +0300

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,22 +1,22 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'SDG AL Lab,
+        jobtitle: 'Front-End Support for Digital Technologies Radar Development',
+        startYear: '2023',
+        endYear: '2023'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'ManyaNet Solutions',
+        jobtitle: 'Network Administrator',
+        startYear: '2022',
+        endYear: '2022'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
+        company: 'Ministry Of Education, Kenya (MOE)',
+        jobtitle: 'Resident Mechanical and Electrical Engineer',
         startYear: '2020',
         endYear: 'Present'
     },
