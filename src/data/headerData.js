d865f93bdb281b51f3f086c441972f1a3bf3579b
Azubi forex <noel.osiro@azubiafrica.org>
2024-02-17 05:33:31 +0300

    push

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Noel Osiro',
     title: "Software Engineer",
     desciption:"A full-stack software engineer specializing in AWS cloud combines frontend and backend development skills, proficiently using various programming languages and frameworks. They excel in creating seamless user experiences and deploying scalable, secure cloud infrastructure on AWS for efficient software solutions.",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: './profile.jpeg',
     resumePdf: resume
 }
